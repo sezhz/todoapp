7c0d680dbb5541e16f62d76772cd7768d1ace112
Sezhz <sezhzqwe@gmail.com>
2023-12-04 22:37:34 +0200

    created todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,136 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
+import tw from "twin.macro";
+import styled from "@emotion/styled";
+
+const RedButton = styled.button`
+  ${tw`bg-red-500 text-white px-2 py-1 mx-1 my-2 rounded-md`}
+`;
+const GreenButton = styled.button`
+  ${tw`bg-green-500 text-white px-2 py-1 mx-1 my-2 rounded-md`}
+`;
+const BlueButton = styled.button`
+  ${tw`bg-blue-400 text-white px-2 py-1 border border-gray-300 p-1`}
+`;
+
+const Checkbox = styled.input`
+  ${tw`mr-2`}
+`;
+
+interface TodoItemProps {
+  isChecked: boolean;
+}
+
+const TodoItem = styled.li<TodoItemProps>`
+  ${tw`flex justify-between items-center`}
+  ${({ isChecked }) => isChecked && tw`line-through text-gray-500`}
+
+  &:nth-child(odd) {
+    ${tw`bg-gray-200 pr-2 pl-2`}
+  }
+
+  &:nth-child(even) {
+    ${tw`pr-2 pl-2`}
+  }
+`;
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [actions, setActions] = useState<string[]>([]);
+  const [editAction, setEditAction] = useState<number | null>(null);
+  const [editValue, setEditValue] = useState<string>("");
+  const [checkedItems, setCheckedItems] = useState<boolean[]>(
+    new Array(actions.length).fill(false)
+  );
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== "") {
+      setActions((prevTodos) => [...prevTodos, newTodo.trim()]);
+      setCheckedItems((prevChecked) => [...prevChecked, false]);
+      setNewTodo("");
+    }
+  };
+
+  const handleEdit = (index: number) => {
+    setEditAction(index);
+    setEditValue(actions[index]);
+  };
+
+  const handleSaveEdit = (index: number) => {
+    const updatedTodos = [...actions];
+    updatedTodos[index] = editValue.trim();
+    setActions(updatedTodos);
+    setEditAction(null);
+  };
+
+  const handleDelete = (index: number) => {
+    const updatedTodos = actions.filter((_, i) => i !== index);
+    setActions(updatedTodos);
+    setCheckedItems((prevChecked) => prevChecked.filter((_, i) => i !== index));
+  };
+
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{ border: "2px solid rgba(0, 0, 0, 0.320)", padding: "10px" }}
+    >
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="font-bold text-2xl mb-4 p-2 bg-gray-100">Todos</h1>
+        <div className="flex items-center justify-between pb-3">
+          <input
+            type="text"
+            placeholder="Add a new todo"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            className="border border-gray-300 p-1 flex-grow"
+          />
+          <BlueButton onClick={handleAddTodo} className="blue-button">
+            Submit
+          </BlueButton>
+        </div>
+        <ul>
+          {actions.map((todo, index) => (
+            <TodoItem key={index} isChecked={checkedItems[index]}>
+              <Checkbox
+                type="checkbox"
+                className="mr-2"
+                checked={checkedItems[index]}
+                onChange={(e) => {
+                  const newCheckedItems = [...checkedItems];
+                  newCheckedItems[index] = e.target.checked;
+                  setCheckedItems(newCheckedItems);
+                }}
+              />
+              {editAction === index ? (
+                <>
+                  <input
+                    type="text"
+                    value={editValue}
+                    onChange={(e) => setEditValue(e.target.value)}
+                    className="w-full"
+                  />
+                  <div className="flex items-center">
+                    <GreenButton onClick={() => handleSaveEdit(index)}>
+                      Save
+                    </GreenButton>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div>{todo}</div>
+                  <div className="flex items-center">
+                    <GreenButton onClick={() => handleEdit(index)}>
+                      Edit
+                    </GreenButton>
+                    <RedButton onClick={() => handleDelete(index)}>
+                      Delete
+                    </RedButton>
+                  </div>
+                </>
+              )}
+            </TodoItem>
+          ))}
+        </ul>
       </header>
     </div>
   );
